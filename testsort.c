df6dce1dec75471943d2a9f09268b4d13a2c171b
fanboyong <74704258+fanboyong@users.noreply.github.com>
2020-11-26 23:32:51 +0800

    Update testsort.c

--- testsort.c
+++ testsort.c
@@ -4,14 +4,16 @@
 
 int main(int argc,char *argv[])
 {
-	    int a[N];
-	    printf("冒泡排序结果如下：\n");
-      Bubble(a, N);
-      printf("插入排序结果如下：\n");
-	    Insert(a, N);
-	    printf("选择排序结果如下：\n");
-	    Select(a, N);
-	    printf("快速排序结果如下：\n");
-	    Quick(a, N);                                                                     
-      return 0;
+	int a[N];
+	printf("原排序结果如下：\n");
+	OutA(a,N)
+	printf("冒泡排序结果如下：\n");
+        Bubble(a, N);
+        printf("插入排序结果如下：\n");
+	Insert(a, N);
+	printf("选择排序结果如下：\n");
+	Select(a, N);
+	printf("快速排序结果如下：\n");
+	Quicksort(a, N);                                                                     
+        return 0;
 }
