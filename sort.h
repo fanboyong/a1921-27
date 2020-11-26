9d15be65cc06ed44d8fe50e52f1028951f5ea039
fanboyong <74704258+fanboyong@users.noreply.github.com>
2020-11-26 23:11:03 +0800

    Create sort.h

---
+++ sort.h
@@ -0,0 +1,12 @@
+#ifdef sort_h
+#define sort_h
+#include<stdio.h>
+#include<stdlib.h>
+
+void OutA(int a[],int asize);
+void Bubble(int a[],int asize);
+void insert(int a[],int asize);
+void Select(int a[],int asize);
+void Quick(int a[],int asize);
+
+#endif
